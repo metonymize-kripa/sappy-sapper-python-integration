ddab094d581aa8b505aa7b0c826f283b7100d4bf
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-02-12 07:01:32 -0600

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,4 +4,4 @@
 
 <h1>About this site</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p>What is the shortest path to think of a stock ticker and know its current price? We are trying to answer that. But really, it's an experiment. Using Sapper, Python and Vercel. To stand up a web-service in minutes.</p>
