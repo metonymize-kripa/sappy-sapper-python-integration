59315fcdb14be626a3ba89d4f2004fa9feb88ee4
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-02-15 11:37:45 -0600

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -36,30 +36,36 @@
 
 <script>
 	let ticker = "SPY";
-	let api_output = "🛁";
+	let api_output = {"symbol":"no_symbol"};
+	let py_ret = "None";
 	
 	async function handleKeydown(event) {
 		if (event.key !== 'Tab') return;
 		event.preventDefault();
-		api_output = "⏳";
 		runAPI();
 	}
 		
 	function runAPI() {
 		fetch("./api/test?sym="+ticker)
 			.then(d => d.text())
-			.then(d => (api_output = d));
+			.then(d => (api_output = JSON.parse(d)));
 	}
 </script>
 
-
 <input bind:value={ticker} on:keydown={handleKeydown}>
 <button on:click={runAPI}>
-	Tab/Clk
+	Tab or Clk
 </button>
 
-<h2><strong>{api_output}</strong></h2>
-
+{#if api_output.symbol == "no_symbol"}
+	<p>Please enter a symbol to see the range of price the stock can take in a week</p>
+{:else if api_output.symbol == "invalid_symbol"}
+	<h3>Your symbol was invalid. Please try again</h3>
+{:else}
+	<h1>{api_output.symbol} ($ {api_output.price})</h1>
+	<h3>One week range (3:1 odds):</h3>
+	<h2><span style="color:red;">${api_output.low}</span> - ${api_output.high}</h2>
+{/if}
 <figure>
 	<img alt='Fat Tony' src='FatTony.png'>
 	<figcaption>Fat Tony: I don't get mad. I get stabby.</figcaption>
