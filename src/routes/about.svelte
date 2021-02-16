1ffa70b0df9253e06b35d9291fb8816ae97b73dd
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-02-16 09:33:47 -0600

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -7,7 +7,11 @@
 <p>What is the shortest path to think of a stock ticker and know its price outlook? We are trying to answer that. But really, it's an experiment. Using Sapper, Python and Vercel. To stand up an immediately useful light weight web-service in minutes.</p>
 
 <ul>
-  <li>Context: Options data is complex, hard to turn into quick tradeable signals</li>
-  <li>Idea: FatNeo range, in seconds, takes 10 minutes of painful options chain analysis out; Comparable market solutions cost $$s/month</li>
-  <li>Use case: We've been testing it out to scope and trade options on our personal accounts in minutes (1 Wk ahead $20 GME Put when FatNeo range was $30-50, 1 Wk Covered Calls on MSFT, etc.)</li>
+  <li><strong>Context:</strong> Options data is complex, hard to turn into quick tradeable signals</li>
+  <li><strong>Idea:</strong> FatNeo range, in seconds, takes 10 minutes of painful options chain analysis out; Comparable market solutions cost $$s/month</li>
+  <li><strong>Use case:</strong> We've been testing it out to scope and trade options on our personal accounts in minutes (1 Wk ahead $20 GME Put when FatNeo range was $30-50, 1 Wk Covered Calls on MSFT, etc.)</li>
 </ul>
+
+<p>But really, it's an experiment. Using Sapper, Python and Vercel. To stand up an immediately useful light weight web-service in minutes.</p>
+
+<p><strong>Test and learn feedback:<a href='https://oracled.mailchimpsites.com/'>here</a></strong></p>
