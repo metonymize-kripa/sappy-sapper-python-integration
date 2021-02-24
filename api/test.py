82cd9ec0730d8716644b6b30aacfcb693f6eade9
Harmeet Goindi <goindi@gmail.com>
2021-02-24 18:39:56 -0500

    cleaninga nd merging

--- api/test.py
+++ api/test.py
@@ -3,11 +3,14 @@
 # https://towardsdatascience.com/how-to-deploy-a-python-serverless-function-to-vercel-f43c8ca393a0
 
 from datetime import datetime, timedelta
-from wallstreet import Call, Put, Stock
+#from wallstreet import Call, Put, Stock
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-from scipy.stats import norm
+import requests
+#from scipy.stats import norm
 import math
+API_URL = 'https://www.insuremystock.com/'
+SKILL_MAP = ['range':'options/range/', 'ape':'options/kelly/', 'doom':'options/doom/' , 'volume':'stocks/volume/', 'prob_pct':'options/prob_pct/']
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -17,123 +20,203 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/plain')
         self.end_headers()
         curr_date = str(datetime.date(datetime.now()))
-        
-        if "sym" in dic:
-            if dic["sym"] == 'WTF':
+        api_output = {'main_point':'',
+                      'main_class':'neutral',
+                      'description':'',
+                      'supporting_data':'',
+                      'secondary_point':'',
+                      'secondary_class':'neutral',
+                      'secondary_description':'' }
+
+        if "input_cmd" in dic:
+            if dic["input_cmd"] == 'WTF':
                 message = '{"symbol":"H🥚dl."}'
             else:
+                cmd_list = dic["input_cmd"].strip().split()
+                skill = "range"
+                symbol = cmd_list[0].lower()
+                if len(cmd_list) > 1:
+                    skill = cmd_list[0].lower()
                 try:
-                    my_stock = Stock(dic["sym"])
-                    my_tuple = get_atm_ivol(my_stock, 7)
-                    curr_price = my_stock.price
-                    low = curr_price - 1.15*my_tuple[1]*curr_price
-                    high = curr_price + 1.15*my_tuple[1]*curr_price
-                    prob_move = get_probability_move(dic["sym"], 7,0)["prob_up"]
-                    message = f'{{"symbol":"{my_stock.ticker}", "prob_up":{prob_move}, "price":"{round(my_stock.price)}","low":"{round(low)}","high":"{round(high)}"}}'
+                    if skill == 'range':
+                        api_output = make_range_response(symbol, api_output)
+                    elif skill == 'doom'
+                        api_output = make_doom_response(symbol, api_output)
+                    elif skill == 'volume'
+                        api_output = make_volume_response(symbol, api_output)
+                    elif skill == 'ape'
+                        api_output = make_ape_response(symbol, api_output)
+                    else:
+                        api_output['main_point'] = f"Invalid Command - {skill}"
+                    #message = f'{{"symbol":"{my_stock.ticker}", "prob_up":{prob_move}, "price":"{round(my_stock.price)}","low":"{round(low)}","high":"{round(high)}"}}'
                 except:
-                    message = '{"symbol":"invalid_symbol"}'
+                    api_output['main_point'] = f"Invalid Command - {skill}"
         else:
-            message = '{"symbol":"invalid_symbol"}'
+            api_output['main_point'] = f"Bro, you need to type in sumthin"
         self.wfile.write(message.encode())
         return
-def get_expiries_bracket(ticker, num_of_days):
-    c = Call(ticker)
-    expiries = c.expirations
-    curr_date = str(datetime.date(datetime.now()))
-    longer_expiry = expiries[-1]
-    shorter_expiry = expiries[0]
-    shorter_day_bound = (datetime.strptime(shorter_expiry,'%d-%m-%Y') - datetime.strptime(curr_date,'%Y-%m-%d')).days
-    longer_day_bound = (datetime.strptime(longer_expiry,'%d-%m-%Y') - datetime.strptime(curr_date,'%Y-%m-%d')).days
-    for i in expiries:
-        days_to_exp = abs(datetime.strptime(i,'%d-%m-%Y') - datetime.strptime(curr_date,'%Y-%m-%d')).days
-        if days_to_exp < num_of_days and days_to_exp > shorter_day_bound  :
-            shorter_day_bound = days_to_exp
-            shorter_expiry = i
-            longer_day_bound = days_to_exp
-            longer_expiry = i
-        elif days_to_exp >= num_of_days:
-            longer_day_bound = days_to_exp
-            longer_expiry = i
-            break;
-    shorter_weight = 1;
-    if longer_day_bound != shorter_day_bound:
-        shorter_weight = (longer_day_bound - num_of_days) / (longer_day_bound - shorter_day_bound)
-    return {'shorter_expiry':shorter_expiry,'longer_expiry':longer_expiry,'shorter_day_bound':shorter_day_bound,'longer_day_bound':longer_day_bound, 'shorter_weight':shorter_weight}
+        
+def make_range_response(symbol, resp_dict):
+    api_end_point = f"{API_URL}{SKILL_MAP['range']}{symbol}"
+    resp = requests.get(api_end_point)
+    if resp.ok: #Good response from FastAPI
+        input_dict = resp.json()
+        resp_dict['main_point'] = f'${round(input_dict["low_range"])} - ${round(input_dict["high_range"])}'
+        resp_dict['description'] = '1Wk Price Band, Options implied @ 75% Prb.'
+        if input_dict['prob_up' > 0.6]:
+            resp_dict['main_class'] = 'bullish'
+        elif input_dict['prob_up' < 0.4]:
+            resp_dict['main_class'] = 'bearish'
+        resp_dict['supporting_data'] = f'Now@ ${round(input_dict["price"])}'
+        resp_dict['secondary_point'] = f'{input_dict["volume"]/input_dict["avg_10d_volume"]:.2f}x adv (input_dict["volume_pct"] %ile)'
+        if input_dict['volume_pct' > 55]:
+            resp_dict['secondary_class'] =  = 'bullish'
+        elif input_dict['volume_pct' < 45]:
+            resp_dict['secondary_class'] = 'bearish'
+        resp_dict['secondary_description'] =  'Current relative volume'
+    return resp_dict
 
-def get_strike_bracket(call_object, price_target):
-    strikes = call_object.strikes
-    higher_strike = strikes[0]
-    lower_strike = strikes[0]
-    if price_target < strikes[0]:
-        return (-1,-1)
-    if price_target > strikes[-1]:
-        return (-10,-10)
-    for i in strikes:
-        if i < price_target and i > lower_strike:
-            lower_strike = i
-            higher_strike = i
-        elif i >= price_target:
-            higher_strike = i
-            break;
-    lower_weight = 1
-    if higher_strike != lower_strike:
-        lower_weight = (higher_strike - price_target)/(higher_strike - lower_strike)
-    return {'lower_strike':lower_strike,'higher_strike':higher_strike, 'lower_weight':lower_weight}
-def get_atm_ivol(s, ndays=30):
-    symbol = s.ticker
-    expiry_dict = get_expiries_bracket(symbol, ndays)
-    #First Shorter One
-    x = expiry_dict['shorter_expiry']
-    shorter_call = Call(symbol,d=int(x[0:2]),m=int(x[3:5]),y=int(x[6:10]))
-    strike_dict = get_strike_bracket(shorter_call, s.price)
-    shorter_call.set_strike(strike_dict['lower_strike'])
-    lower_vol = shorter_call.implied_volatility()
-    shorter_call.set_strike(strike_dict['higher_strike'])
-    higher_vol = shorter_call.implied_volatility()
-    shorter_ivol = lower_vol*strike_dict['lower_weight'] + higher_vol*(1-strike_dict['lower_weight'])
-    #Now longer One
-    x = expiry_dict['longer_expiry']
-    longer_call = Call(symbol,d=int(x[0:2]),m=int(x[3:5]),y=int(x[6:10]))
-    strike_dict = get_strike_bracket(longer_call, s.price)
-    longer_call.set_strike(strike_dict['lower_strike'])
-    lower_vol = longer_call.implied_volatility()
-    longer_call.set_strike(strike_dict['higher_strike'])
-    higher_vol = longer_call.implied_volatility()
-    longer_ivol = lower_vol*strike_dict['lower_weight'] + higher_vol*(1-strike_dict['lower_weight'])
-    implied_ivol = shorter_ivol*expiry_dict['shorter_weight'] + longer_ivol*(1-expiry_dict['shorter_weight'])
-    one_sigma_move_ndays_day = implied_ivol*math.sqrt(ndays/365)
-    return (implied_ivol, one_sigma_move_ndays_day)
+def make_doom_response(symbol, resp_dict):
+    api_end_point = f"{API_URL}{SKILL_MAP['doom']}{symbol}/?days=30&percent=5"
+    resp = requests.get(api_end_point)
+    if resp.ok: #Good response from FastAPI
+        input_dict = resp.json()
+        resp_dict['main_point'] = f'Crash Index @{round(100*input_dict["prob_down"])}'
+        resp_dict['description'] = 'Options implied Prb. of 5%👇 in month ahead'
+        if input_dict['prob_down' < 0.1]:
+            resp_dict['main_class'] = 'bullish'
+        elif input_dict['prob_down' > 0.2]:
+            resp_dict['main_class'] = 'bearish'
+    return resp_dict
 
-def get_probability_move(symbol:str, n_days:int, percent:float):
-    c = Call(symbol)
-    curr_date = str(datetime.date(datetime.now()))
-    expiries = c.expirations
-    expiry_to_use = expiries[0]
-    for i in expiries:
-        days_to_exp = abs(datetime.strptime(i,'%d-%m-%Y') - datetime.strptime(curr_date,'%Y-%m-%d')).days
-        expiry_to_use = i
-        if days_to_exp >= n_days:
-            break
-    my_delta = get_delta(symbol, percent, expiry_to_use)
-    return {"move_percent":percent, 'expiry':expiry_to_use, "prob_down":my_delta['delta_down'],"prob_up":my_delta['delta_up'] }
+def make_ape_response(symbol, resp_dict):
+    api_end_point = f"{API_URL}{SKILL_MAP['ape']}{symbol}"
+    resp = requests.get(api_end_point)
+    if resp.ok: #Good response from FastAPI
+        input_dict = resp.json()
+        resp_dict['main_point'] = f"Don't put more than {round(input_dict['kelly']*100)}% in this stonk"
+        resp_dict['description'] = """Apes together but heed sage <a href="https://en.wikipedia.org/wiki/Kelly_criterion"> Kelly's advice</a>"""
+    return resp_dict
 
-def get_delta(symbol:str, percent_move:float, expiry:str):
-    s = Stock(symbol)
-    up_px = s.price*(1+percent_move/100)
-    down_px = s.price*(1-percent_move/100)
-    call = Call(symbol,d=int(expiry[0:2]),m=int(expiry[3:5]),y=int(expiry[6:10]))
-    up_delta_dict = get_strike_bracket(call, up_px)
-    call.set_strike(up_delta_dict['lower_strike'])
-    delta1 = call.delta()*up_delta_dict['lower_weight']
-    call.set_strike(up_delta_dict['higher_strike'])
-    delta2 = call.delta()*(1-up_delta_dict['lower_weight'])
-    delta_up_move = delta1 + delta2
+def make_volume_response(symbol, resp_dict):
+    api_end_point = f"{API_URL}{SKILL_MAP['volume']}{symbol}"
+    resp = requests.get(api_end_point)
+    if resp.ok: #Good response from FastAPI
+        input_dict = resp.json()
+        adv_x_volume = input_dict["volume"]/input_dict["avg_10d_volume"]
+        resp_dict['main_point'] = f'{adv_x_volume:.2f}x adv'
+        resp_dict['description'] = 'Relative Volume based on 10 days average'
+        if adv_x_volume > 1:
+            resp_dict['main_class'] = 'bullish'
+        elif adv_x_volume < 0.7:
+            resp_dict['main_class'] = 'bearish'
+        resp_dict['supporting_data'] = f'Now@ {(input_dict["volume"])}'
+        resp_dict['secondary_point'] = input_dict["percentile"]
+        if input_dict['volume_pct' > 55]:
+            resp_dict['secondary_class'] =  = 'bullish'
+        elif input_dict['volume_pct' < 45]:
+            resp_dict['secondary_class'] = 'bearish'
+        resp_dict['secondary_description'] =  'Current volume percentile'
+    return resp_dict
 
-    put = Put(symbol,d=int(expiry[0:2]),m=int(expiry[3:5]),y=int(expiry[6:10]))
-    down_delta_dict = get_strike_bracket(put, down_px)
-    put.set_strike(down_delta_dict['lower_strike'])
-    delta1 = -put.delta()*down_delta_dict['lower_weight']
-    put.set_strike(down_delta_dict['higher_strike'])
-    delta2 = -put.delta()*(1-down_delta_dict['lower_weight'])
-    delta_down_move = delta1 + delta2
-    return {'delta_up':delta_up_move,'delta_down':delta_down_move}
+# def get_expiries_bracket(ticker, num_of_days):
+#     c = Call(ticker)
+#     expiries = c.expirations
+#     curr_date = str(datetime.date(datetime.now()))
+#     longer_expiry = expiries[-1]
+#     shorter_expiry = expiries[0]
+#     shorter_day_bound = (datetime.strptime(shorter_expiry,'%d-%m-%Y') - datetime.strptime(curr_date,'%Y-%m-%d')).days
+#     longer_day_bound = (datetime.strptime(longer_expiry,'%d-%m-%Y') - datetime.strptime(curr_date,'%Y-%m-%d')).days
+#     for i in expiries:
+#         days_to_exp = abs(datetime.strptime(i,'%d-%m-%Y') - datetime.strptime(curr_date,'%Y-%m-%d')).days
+#         if days_to_exp < num_of_days and days_to_exp > shorter_day_bound  :
+#             shorter_day_bound = days_to_exp
+#             shorter_expiry = i
+#             longer_day_bound = days_to_exp
+#             longer_expiry = i
+#         elif days_to_exp >= num_of_days:
+#             longer_day_bound = days_to_exp
+#             longer_expiry = i
+#             break;
+#     shorter_weight = 1;
+#     if longer_day_bound != shorter_day_bound:
+#         shorter_weight = (longer_day_bound - num_of_days) / (longer_day_bound - shorter_day_bound)
+#     return {'shorter_expiry':shorter_expiry,'longer_expiry':longer_expiry,'shorter_day_bound':shorter_day_bound,'longer_day_bound':longer_day_bound, 'shorter_weight':shorter_weight}
+#
+# def get_strike_bracket(call_object, price_target):
+#     strikes = call_object.strikes
+#     higher_strike = strikes[0]
+#     lower_strike = strikes[0]
+#     if price_target < strikes[0]:
+#         return (-1,-1)
+#     if price_target > strikes[-1]:
+#         return (-10,-10)
+#     for i in strikes:
+#         if i < price_target and i > lower_strike:
+#             lower_strike = i
+#             higher_strike = i
+#         elif i >= price_target:
+#             higher_strike = i
+#             break;
+#     lower_weight = 1
+#     if higher_strike != lower_strike:
+#         lower_weight = (higher_strike - price_target)/(higher_strike - lower_strike)
+#     return {'lower_strike':lower_strike,'higher_strike':higher_strike, 'lower_weight':lower_weight}
+# def get_atm_ivol(s, ndays=30):
+#     symbol = s.ticker
+#     expiry_dict = get_expiries_bracket(symbol, ndays)
+#     #First Shorter One
+#     x = expiry_dict['shorter_expiry']
+#     shorter_call = Call(symbol,d=int(x[0:2]),m=int(x[3:5]),y=int(x[6:10]))
+#     strike_dict = get_strike_bracket(shorter_call, s.price)
+#     shorter_call.set_strike(strike_dict['lower_strike'])
+#     lower_vol = shorter_call.implied_volatility()
+#     shorter_call.set_strike(strike_dict['higher_strike'])
+#     higher_vol = shorter_call.implied_volatility()
+#     shorter_ivol = lower_vol*strike_dict['lower_weight'] + higher_vol*(1-strike_dict['lower_weight'])
+#     #Now longer One
+#     x = expiry_dict['longer_expiry']
+#     longer_call = Call(symbol,d=int(x[0:2]),m=int(x[3:5]),y=int(x[6:10]))
+#     strike_dict = get_strike_bracket(longer_call, s.price)
+#     longer_call.set_strike(strike_dict['lower_strike'])
+#     lower_vol = longer_call.implied_volatility()
+#     longer_call.set_strike(strike_dict['higher_strike'])
+#     higher_vol = longer_call.implied_volatility()
+#     longer_ivol = lower_vol*strike_dict['lower_weight'] + higher_vol*(1-strike_dict['lower_weight'])
+#     implied_ivol = shorter_ivol*expiry_dict['shorter_weight'] + longer_ivol*(1-expiry_dict['shorter_weight'])
+#     one_sigma_move_ndays_day = implied_ivol*math.sqrt(ndays/365)
+#     return (implied_ivol, one_sigma_move_ndays_day)
+#
+# def get_probability_move(symbol:str, n_days:int, percent:float):
+#     c = Call(symbol)
+#     curr_date = str(datetime.date(datetime.now()))
+#     expiries = c.expirations
+#     expiry_to_use = expiries[0]
+#     for i in expiries:
+#         days_to_exp = abs(datetime.strptime(i,'%d-%m-%Y') - datetime.strptime(curr_date,'%Y-%m-%d')).days
+#         expiry_to_use = i
+#         if days_to_exp >= n_days:
+#             break
+#     my_delta = get_delta(symbol, percent, expiry_to_use)
+#     return {"move_percent":percent, 'expiry':expiry_to_use, "prob_down":my_delta['delta_down'],"prob_up":my_delta['delta_up'] }
+#
+# def get_delta(symbol:str, percent_move:float, expiry:str):
+#     s = Stock(symbol)
+#     up_px = s.price*(1+percent_move/100)
+#     down_px = s.price*(1-percent_move/100)
+#     call = Call(symbol,d=int(expiry[0:2]),m=int(expiry[3:5]),y=int(expiry[6:10]))
+#     up_delta_dict = get_strike_bracket(call, up_px)
+#     call.set_strike(up_delta_dict['lower_strike'])
+#     delta1 = call.delta()*up_delta_dict['lower_weight']
+#     call.set_strike(up_delta_dict['higher_strike'])
+#     delta2 = call.delta()*(1-up_delta_dict['lower_weight'])
+#     delta_up_move = delta1 + delta2
+#
+#     put = Put(symbol,d=int(expiry[0:2]),m=int(expiry[3:5]),y=int(expiry[6:10]))
+#     down_delta_dict = get_strike_bracket(put, down_px)
+#     put.set_strike(down_delta_dict['lower_strike'])
+#     delta1 = -put.delta()*down_delta_dict['lower_weight']
+#     put.set_strike(down_delta_dict['higher_strike'])
+#     delta2 = -put.delta()*(1-down_delta_dict['lower_weight'])
+#     delta_down_move = delta1 + delta2
+#     return {'delta_up':delta_up_move,'delta_down':delta_down_move}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,8 +14,10 @@
 		text-transform: uppercase;
 		font-weight: 400;
 		margin: 0 auto;
+
 		width:60%;
-		
+
+
 	}
 	button {
 		text-transform: uppercase;
@@ -47,7 +49,7 @@
 	p {
 		margin: 1em auto;
 	}
-	
+
 	progress {
 		display: block;
 		width: 200px;
@@ -56,7 +58,7 @@
 	}
 	.bear, .bull, .neutral {
 		text-align: center;
-		margin: -10px auto;
+
 		display:block;
 	}
 	.bear{
@@ -65,30 +67,20 @@
 	.bull{
 		color:green;
 	}
-	
-	Autocomplete{
-		width:60%;
-	}
-	.autocomplete-input{
-		width:60%;
-	}
-	.input .autocomplete-input{
-		width:60%;
-	}
-	
+
 	/*.progress-div {
 	    width: 200px;
 	    height: 40px;
 	    align-items: center;
 	    text-align: center;
-	    margin: 0 auto;	
+	    margin: 0 auto;
 	}
 	.progress-label{
 	    position: absolute;
 	    display: block;
 	    margin: -32px 90px;
 	}
-	
+
 	progress:before {
 	  content: attr(data-label);
 	  font-size: 1em;
@@ -98,7 +90,7 @@
 	  font-weight: 800;
 	}
 	*/
-	
+
 	@media (min-width: 480px) {
 		h1 {
 			font-size: 4em;
@@ -112,19 +104,20 @@
 	import AutoComplete from "simple-svelte-autocomplete";
 	const tickers = ["AAL","AAL DOOM","AAL KELLY","AAL WSB","AAL VOLUME","AAL NEW2","AAPL","AAPL DOOM","AAPL KELLY","AAPL WSB","AAPL VOLUME","AAPL NEW2","ALXN","ALXN DOOM","ALXN KELLY","ALXN WSB","ALXN VOLUME","ALXN NEW2","AMC","AMC DOOM","AMC KELLY","AMC WSB","AMC VOLUME","AMC NEW2","AMD","AMD DOOM","AMD KELLY","AMD WSB","AMD VOLUME","AMD NEW2","AMZN","AMZN DOOM","AMZN KELLY","AMZN WSB","AMZN VOLUME","AMZN NEW2","APHA","APHA DOOM","APHA KELLY","APHA WSB","APHA VOLUME","APHA NEW2","ARKF","ARKF DOOM","ARKF KELLY","ARKF WSB","ARKF VOLUME","ARKF NEW2","ARKK","ARKK DOOM","ARKK KELLY","ARKK WSB","ARKK VOLUME","ARKK NEW2","BABA","BABA DOOM","BABA KELLY","BABA WSB","BABA VOLUME","BABA NEW2","BAC","BAC DOOM","BAC KELLY","BAC WSB","BAC VOLUME","BAC NEW2","BB","BB DOOM","BB KELLY","BB WSB","BB VOLUME","BB NEW2","BKNG","BKNG DOOM","BKNG KELLY","BKNG WSB","BKNG VOLUME","BKNG NEW2","BRK/B","BRK/B DOOM","BRK/B KELLY","BRK/B WSB","BRK/B VOLUME","BRK/B NEW2","C","C DOOM","C KELLY","C WSB","C VOLUME","C NEW2","CCIV","CCIV DOOM","CCIV KELLY","CCIV WSB","CCIV VOLUME","CCIV NEW2","CCL","CCL DOOM","CCL KELLY","CCL WSB","CCL VOLUME","CCL NEW2","CFO","CFO DOOM","CFO KELLY","CFO WSB","CFO VOLUME","CFO NEW2","CHNG","CHNG DOOM","CHNG KELLY","CHNG WSB","CHNG VOLUME","CHNG NEW2","CHTR","CHTR DOOM","CHTR KELLY","CHTR WSB","CHTR VOLUME","CHTR NEW2","CRM","CRM DOOM","CRM KELLY","CRM WSB","CRM VOLUME","CRM NEW2","CRSR","CRSR DOOM","CRSR KELLY","CRSR WSB","CRSR VOLUME","CRSR NEW2","CVNA","CVNA DOOM","CVNA KELLY","CVNA WSB","CVNA VOLUME","CVNA NEW2","CZR","CZR DOOM","CZR KELLY","CZR WSB","CZR VOLUME","CZR NEW2","DIS","DIS DOOM","DIS KELLY","DIS WSB","DIS VOLUME","DIS NEW2","DKNG","DKNG DOOM","DKNG KELLY","DKNG WSB","DKNG VOLUME","DKNG NEW2","E","E DOOM","E KELLY","E WSB","E VOLUME","E NEW2","EOD","EOD DOOM","EOD KELLY","EOD WSB","EOD VOLUME","EOD NEW2","EV","EV DOOM","EV KELLY","EV WSB","EV VOLUME","EV NEW2","EXPE","EXPE DOOM","EXPE KELLY","EXPE WSB","EXPE VOLUME","EXPE NEW2","FB","FB DOOM","FB KELLY","FB WSB","FB VOLUME","FB NEW2","FDX","FDX DOOM","FDX KELLY","FDX WSB","FDX VOLUME","FDX NEW2","FE","FE DOOM","FE KELLY","FE WSB","FE VOLUME","FE NEW2","FIS","FIS DOOM","FIS KELLY","FIS WSB","FIS VOLUME","FIS NEW2","FISV","FISV DOOM","FISV KELLY","FISV WSB","FISV VOLUME","FISV NEW2","GDDY","GDDY DOOM","GDDY KELLY","GDDY WSB","GDDY VOLUME","GDDY NEW2","GE","GE DOOM","GE KELLY","GE WSB","GE VOLUME","GE NEW2","GME","GME DOOM","GME KELLY","GME WSB","GME VOLUME","GME NEW2","GOOGL","GOOGL DOOM","GOOGL KELLY","GOOGL WSB","GOOGL VOLUME","GOOGL NEW2","GPN","GPN DOOM","GPN KELLY","GPN WSB","GPN VOLUME","GPN NEW2","HCA","HCA DOOM","HCA KELLY","HCA WSB","HCA VOLUME","HCA NEW2","IAC","IAC DOOM","IAC KELLY","IAC WSB","IAC VOLUME","IAC NEW2","ICLN","ICLN DOOM","ICLN KELLY","ICLN WSB","ICLN VOLUME","ICLN NEW2","IMO","IMO DOOM","IMO KELLY","IMO WSB","IMO VOLUME","IMO NEW2","IPO","IPO DOOM","IPO KELLY","IPO WSB","IPO VOLUME","IPO NEW2","JD","JD DOOM","JD KELLY","JD WSB","JD VOLUME","JD NEW2","JPM","JPM DOOM","JPM KELLY","JPM WSB","JPM VOLUME","JPM NEW2","LBRDK","LBRDK DOOM","LBRDK KELLY","LBRDK WSB","LBRDK VOLUME","LBRDK NEW2","LSXMK","LSXMK DOOM","LSXMK KELLY","LSXMK WSB","LSXMK VOLUME","LSXMK NEW2","MA","MA DOOM","MA KELLY","MA WSB","MA VOLUME","MA NEW2","MELI","MELI DOOM","MELI KELLY","MELI WSB","MELI VOLUME","MELI NEW2","MRO","MRO DOOM","MRO KELLY","MRO WSB","MRO VOLUME","MRO NEW2","MSFT","MSFT DOOM","MSFT KELLY","MSFT WSB","MSFT VOLUME","MSFT NEW2","MU","MU DOOM","MU KELLY","MU WSB","MU VOLUME","MU NEW2","NFLX","NFLX DOOM","NFLX KELLY","NFLX WSB","NFLX VOLUME","NFLX NEW2","NIO","NIO DOOM","NIO KELLY","NIO WSB","NIO VOLUME","NIO NEW2","NOK","NOK DOOM","NOK KELLY","NOK WSB","NOK VOLUME","NOK NEW2","NOW","NOW DOOM","NOW KELLY","NOW WSB","NOW VOLUME","NOW NEW2","NVDA","NVDA DOOM","NVDA KELLY","NVDA WSB","NVDA VOLUME","NVDA NEW2","NYC","NYC DOOM","NYC KELLY","NYC WSB","NYC VOLUME","NYC NEW2","OSK","OSK DOOM","OSK KELLY","OSK WSB","OSK VOLUME","OSK NEW2","PINS","PINS DOOM","PINS KELLY","PINS WSB","PINS VOLUME","PINS NEW2","PLTR","PLTR DOOM","PLTR KELLY","PLTR WSB","PLTR VOLUME","PLTR NEW2","PLUG","PLUG DOOM","PLUG KELLY","PLUG WSB","PLUG VOLUME","PLUG NEW2","PT","PT DOOM","PT KELLY","PT WSB","PT VOLUME","PT NEW2","PTON","PTON DOOM","PTON KELLY","PTON WSB","PTON VOLUME","PTON NEW2","PYPL","PYPL DOOM","PYPL KELLY","PYPL WSB","PYPL VOLUME","PYPL NEW2","QCOM","QCOM DOOM","QCOM KELLY","QCOM WSB","QCOM VOLUME","QCOM NEW2","QQQ","QQQ DOOM","QQQ KELLY","QQQ WSB","QQQ VOLUME","QQQ NEW2","R","R DOOM","R KELLY","R WSB","R VOLUME","R NEW2","RIOT","RIOT DOOM","RIOT KELLY","RIOT WSB","RIOT VOLUME","RIOT NEW2","RKT","RKT DOOM","RKT KELLY","RKT WSB","RKT VOLUME","RKT NEW2","SE","SE DOOM","SE KELLY","SE WSB","SE VOLUME","SE NEW2","SNAP","SNAP DOOM","SNAP KELLY","SNAP WSB","SNAP VOLUME","SNAP NEW2","SNDL","SNDL DOOM","SNDL KELLY","SNDL WSB","SNDL VOLUME","SNDL NEW2","SPY","SPY DOOM","SPY KELLY","SPY WSB","SPY VOLUME","SPY NEW2","SQ","SQ DOOM","SQ KELLY","SQ WSB","SQ VOLUME","SQ NEW2","TDG","TDG DOOM","TDG KELLY","TDG WSB","TDG VOLUME","TDG NEW2","THCB","THCB DOOM","THCB KELLY","THCB WSB","THCB VOLUME","THCB NEW2","TLRY","TLRY DOOM","TLRY KELLY","TLRY WSB","TLRY VOLUME","TLRY NEW2","TMUS","TMUS DOOM","TMUS KELLY","TMUS WSB","TMUS VOLUME","TMUS NEW2","TSLA","TSLA DOOM","TSLA KELLY","TSLA WSB","TSLA VOLUME","TSLA NEW2","TWTR","TWTR DOOM","TWTR KELLY","TWTR WSB","TWTR VOLUME","TWTR NEW2","UBER","UBER DOOM","UBER KELLY","UBER WSB","UBER VOLUME","UBER NEW2","UPWK","UPWK DOOM","UPWK KELLY","UPWK WSB","UPWK VOLUME","UPWK NEW2","UVXY","UVXY DOOM","UVXY KELLY","UVXY WSB","UVXY VOLUME","UVXY NEW2","V","V DOOM","V KELLY","V WSB","V VOLUME","V NEW2","VAR","VAR DOOM","VAR KELLY","VAR WSB","VAR VOLUME","VAR NEW2","WFC","WFC DOOM","WFC KELLY","WFC WSB","WFC VOLUME","WFC NEW2","WKHS","WKHS DOOM","WKHS KELLY","WKHS WSB","WKHS VOLUME","WKHS NEW2"];
 	let selectedTicker = "AAL";
-	
+
 	const progress = tweened(0, {
 		duration: 400,
 		easing: cubicOut
 	});
-	
-	let ticker = "AAL";
+
+	let ticker = "SPY";
+
 	let api_output = {"symbol":"no_symbol"};
 	let volume_output = {"symbol":"no_symbol"};
 	let doom_output = {"symbol":"no_symbol"};
 	let ape_output = {"symbol":"no_symbol"};
 	let cmd_used = "range";
-	
+
 	async function handleKeydown(event) {
 		if (event.key === 'Enter') {
 			//event.preventDefault();
@@ -134,9 +127,9 @@
 			 return;
 		}
 	}
-		
+
 	function runAPI() {
-		
+
 		var tx_array = ticker.trim().split(/\s+/);
 		if (tx_array.length > 1)
 		{
@@ -149,7 +142,7 @@
 				.then(response => response.json())
 				.then(data=>volume_output=data)
 				.then(x => progress.set(volume_output.percentile/100));
-				
+
 			}
 			else if (tx_array[1].toLowerCase() == 'doom')
 			{
@@ -165,7 +158,7 @@
 			{
 				cmd_used = 'ape';
 				ape_output.kelly = '-';
-				
+
 				fetch("https://www.insuremystock.com/options/kelly/"+tx_array[0])
 				.then(response => response.json())
 				.then(data=>ape_output=data)
@@ -176,24 +169,24 @@
 				cmd_used = 'invalid';
 			}
 		}
-		
+
 		else {
 			api_output = {"symbol":"waiting"};
 			cmd_used = "range";
 			progress.set(0);
 			volume_output.percentile = '-';
-			fetch("./api/test?sym="+ticker)
+			fetch("./api/test?input_cmd="+ticker)
 				.then(d => d.text())
 				.then(d => (api_output = JSON.parse(d)))
 				.then(d => console.log(d));
-			
+
 			fetch("https://www.insuremystock.com/stocks/volume/"+tx_array[0])
 			.then(response => response.json())
 			.then(data=>volume_output=data)
 			.then(x => progress.set(volume_output.percentile/100));
 		}
-		
-		
+
+
 	}
 </script>
 
@@ -204,7 +197,17 @@
 	</button>
 </div>
 
-{#if cmd_used == "range"}
+{# if api_output.symbol == "waiting"}
+    <p>Getting results.....</p>
+{:else}
+    <h2><span style="class:{api_output.main_class};">{api_output.main_point}</h2>
+    <p>{api_output.description}</p>
+    <h3>{supporting_data}</h3>
+    <h2><span style="class:{api_output.secondary_class};">{api_output.secondary_point}</h2>
+    <p>{api_output.secondary_description}</p>
+{/if}
+
+<!--{#if cmd_used == "range"}
 	{#if api_output.symbol == "no_symbol"}
 		<p>Type Ticker then Tab/Click.</p>
 	{:else if api_output.symbol == "H🥚dl."}
@@ -240,7 +243,7 @@
 	{:else}
 		<span class="neutral">Current relative volume level</span>
 	{/if}
-	
+
 	<p>Current stock volume rank based on past 2 weeks</p>
 {:else if cmd_used == "ape"}
 	<p> Experimental feature </p>
@@ -257,14 +260,14 @@
 		<span class="bear">Crash Index @ {Math.round(doom_output.prob_down*100)}</span>
 	{/if}
 	<p>Options implied Prb. of 5%👇 in month ahead</p>
-	
+
 
 {:else }
-	
+
 	<h2><span style="color:red;">INVALID COMMAND</span></h2>
 
 {/if}
-
+-->
 <figure>
 	<img alt='Fat Tony' src='FatTony.png'>
 	<figcaption>Fat Tony: I don't get mad. I get stabby.</figcaption>
