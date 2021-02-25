dded28af1169060d34aa16433659112684da7728
Harmeet Goindi <goindi@gmail.com>
2021-02-24 21:30:30 -0500

    merges

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -128,13 +128,13 @@
 			progress.set(0);
 			fetch("./api/test?input_cmd="+ticker)
 				.then(d => d.text())
-                .then(d => console.log(d));
+                .then(d => console.log(d))
 				.then(d => (api_output = JSON.parse(d)));
 
 	}
 </script>
 
-<div style='margin: 10 auto;'>
+<div>
 	<AutoComplete items={tickers} bind:selectedItem={ticker} on:keydown={handleKeydown} maxItemsToShowInList=5/>
 	<button on:click={runAPI}>
 		GO
