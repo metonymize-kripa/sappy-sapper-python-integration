45b5060ad2336d779816cb000a5d6cc1dad907ca
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-02-25 11:21:55 -0600

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -150,7 +150,7 @@
 </script>
 
 <div>
-	<AutoComplete  class="my-ac" items={tickers} bind:selectedItem={ticker} on:keydown={handleKeydown} maxItemsToShowInList=5 hideArrow=true/>
+	<AutoComplete  class="my-ac" items={tickers} bind:selectedItem={ticker} on:keydown={handleKeydown} maxItemsToShowInList=5 hideArrow=true placeholder="SPY" />
 	<button on:click={runAPI}>
 		GO
 	</button>
