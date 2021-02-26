fa6bbd36059879f55cd3301b259eb4ae53f7ebc0
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-02-26 09:17:30 -0600

    Update usecases.svelte

--- src/routes/usecases.svelte
+++ src/routes/usecases.svelte
@@ -7,44 +7,7 @@
 <p>Options are just one of the many sources of insights that we make available for traders.
 Here are a few use cases to inspire your exploration with our tools. Be bold. Hope you have fun!</p>
 
-<script>
-	import { onMount } from 'svelte';
-
-	let photos = [];
-
-	onMount(async () => {
-		const res = await fetch(`https://jsonplaceholder.typicode.com/photos?_limit=9`);
-		photos = await res.json();
-	});
-</script>
-
-<style>
-	.photos {
-		width: 100%;
-		display: grid;
-		grid-template-columns: repeat(3, 1fr);
-		grid-gap: 8px;
-	}
-
-	figure, img {
-		width: 100%;
-		margin: 0;
-	}
-</style>
-
 <h1>✨ FatNeo's Wall of Fame [Placeholder in works]</h1>
-
-<div class="photos">
-	{#each photos as photo}
-		<figure>
-			<img src={photo.thumbnailUrl} alt={photo.title}>
-			<figcaption>{photo.title}</figcaption>
-		</figure>
-	{:else}
-		<!-- this block renders when photos.length === 0 -->
-		<p>loading...</p>
-	{/each}
-</div>
 
 <h2><strong>Avoiding melt down</strong></h2>
 <p>Using TICKER DOOM allows us to estimate the options implied probability of a sharp near term drop.</p>
