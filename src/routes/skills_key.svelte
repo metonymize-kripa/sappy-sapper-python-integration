800a73f2af4802a808174aa05297274ac87f3b12
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-02-27 03:57:27 -0600

    Update skills_key.svelte

--- src/routes/skills_key.svelte
+++ src/routes/skills_key.svelte
@@ -2,19 +2,20 @@
 	<title>Key to Fat Neo's "skills"</title>
 </svelte:head>
 
-<h1>By skill, we mean e.g. SPY _SKILL_ ↩️</h1>
+<h1>Skills, the Key</h1>
 
-<p>What are all the skills that Fat Neo has? How do they work? We try to answer that below briefly, 
-    with links to more detailed sources and methodology in the blog section. </p>
+<p>By a Fat Neo skill we mean something like "SPY _SKILL_ ↩️". It is how Fat Neo helps you trade faster, smarter, funner.</p>
+<p>What are all the skills that Fat Neo has? How do they work? We try to answer these questions briefly on this page, 
+    with links to more detailed sources and methodology that you can find in the blog section. </p>
 
 <ul>
-  <li><strong>       :</strong> Entering only _TICKER_ (e.g. SPY ↩️) is the default command, gets you a basic Options Implied price outlook for the week ahead</li>
-  <li><strong>💥 DOOM:</strong> Entering _TICKER_ DOOM is gets you the Options Implied probability of a strong drop in the month ahead</li>
-  <li><strong>🎲 KELLY:</strong> Entering _TICKER_ KELLY is an experimental feature, that does optimal Kelly bet sizing. Perhaps one of the most overlooked tool by investors.</li>
-  <li><strong>📈 CALL:</strong> Entering _TICKER_ CALL is an experimental feature, helps sort and find the optimal covered call to sell in the month ahead.</li>
-  <li><strong>🔊 VOLUME:</strong> Entering _TICKER_ VOLUME gets you a basic comparative view of stock volume vs average levels experienced recently</li>
-  <li><strong>🎢 WSB:</strong> Entering _TICKER_ WSB is an experimental feature, gets you a relative feel for volume of mentions on Reddit's WallStreetBets</li>
-    <li><strong>📯 _YOURS_:</strong> We are just getting started. Most of the skills above came from users like you. Connect below.</li>
+  <li><strong> __:</strong> Entering only _TICKER_ (e.g. SPY ↩️) is the default command. It gets you a basic Options Implied price outlook for the week ahead</li>
+  <li><strong>💥 DOOM:</strong> Entering _TICKER_ DOOM gets you the Options Implied probability of a strong drop in the month ahead</li>
+  <li><strong>🎲 KELLY:</strong> Entering _TICKER_ KELLY is an experimental feature, to do Options Implied Kelly optimal bet sizing. Perhaps one of the most overlooked tool by investors.</li>
+  <li><strong>📈 CALL:</strong> Entering _TICKER_ CALL is an experimental feature, to find the optimal covered call/call-spread to sell in the month ahead</li>
+  <li><strong>🔊 VOLUME:</strong> Entering _TICKER_ VOLUME gets you a basic comparative view of stock volume vs the recent average</li>
+  <li><strong>🎢 WSB:</strong> Entering _TICKER_ WSB is an experimental feature, offering a directional feel for sentiment on Reddit's WallStreetBets (e.g. Mentions)</li>
+    <li><strong>📯 _USER_X_:</strong> We are just getting started and are all ears. Most of the skills above came from users like you. Connect below.</li>
 </ul>
 
 <p><strong>Test and learn feedback:<a href='https://oracled.mailchimpsites.com/'>here</a></strong></p>
