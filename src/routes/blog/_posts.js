c1f5121585f46ca5f2b497d05667d8dddf7c4337
Harmeet Goindi <goindi@gmail.com>
2021-02-27 16:39:54 -0500

    comments

--- src/routes/blog/_posts.js
+++ src/routes/blog/_posts.js
@@ -30,7 +30,7 @@ const posts = [
         `
     },
     {
-        title: 'What is <b>range</b>?',
+        title: 'What is range?',
         slug: 'tutorial-range',
         html: `
               Using options data we calculate at-the-money implied volatility and use that as a proxy for standard deviation(sigma). Default range is the one that give us the 75% probabilty. If you want 90% probability range use sigma=1.96.
@@ -38,7 +38,7 @@ const posts = [
           `
      },
     {
-        title: 'What is <b>doom</b>?',
+        title: 'What is doom?',
         slug: 'tutorial-doom',
         html: `
                 Using options data we calculate probability of options expiring in the money. For a call that is eqivalent to the probability of stock being over strike and for the put the prob of stock being below the strike. Doom gives us the probability of put expiring in the money.
@@ -46,7 +46,7 @@ const posts = [
             `
     },
     {
-        title: 'What is <b>volume</b>?',
+        title: 'What is volume?',
         slug: 'tutorial-volume',
         html: `
                   Using stock volume from past 10 days, we calculate the current level in two ways - as a multiple of average daily volume and as a percentile. Volume numbers during a trading session are scaled appropriately.
@@ -54,7 +54,7 @@ const posts = [
               `
     },
     {
-        title: 'What is <b>call</b>?',
+        title: 'What is call?',
         slug: 'tutorial-call',
         html: `
                 Using options data we calculate the probabilty of a call option expiring in the money. That probabilty along with premium received gives us an idea of which option has the maximum payoff potential. Similar methodology is used to find the optimal spread trade.
@@ -62,7 +62,7 @@ const posts = [
               `
     },
     {
-        title: 'What is <b>kelly</b>?',
+        title: 'What is kelly?',
         slug: 'tutorial-kelly',
         html: `
                 Using at-the-money options data we calculate the options implied probabilty of stock going up.  Based on those probabilty we calculate the optimal betting size of stock using Kelly criterion
--- src/routes/skills_key.svelte
+++ src/routes/skills_key.svelte
@@ -5,15 +5,15 @@
 <h1>Skills, the Key</h1>
 
 <p>By a Fat Neo skill we mean something like "SPY _SKILL_ ↩️". It is how Fat Neo helps you trade faster, smarter, funner.</p>
-<p>What are all the skills that Fat Neo has? How do they work? We try to answer these questions briefly on this page, 
+<p>What are all the skills that Fat Neo has? How do they work? We try to answer these questions briefly on this page,
     with links to more detailed sources and methodology that you can find in the blog section. </p>
 
 <ul>
-  <li><strong> __:</strong> Entering only _TICKER_ (e.g. SPY ↩️) is the default command. It gets you a basic Options Implied price outlook for the week ahead</li>
-  <li><strong>💥 DOOM:</strong> Entering _TICKER_ DOOM gets you the Options Implied probability of a strong drop in the month ahead</li>
-  <li><strong>🎲 KELLY:</strong> Entering _TICKER_ KELLY is an experimental feature, to do Options Implied Kelly optimal bet sizing. Perhaps one of the most overlooked tool by investors.</li>
-  <li><strong>📈 CALL:</strong> Entering _TICKER_ CALL is an experimental feature, to find the optimal covered call/call-spread to sell in the month ahead</li>
-  <li><strong>🔊 VOLUME:</strong> Entering _TICKER_ VOLUME gets you a basic comparative view of stock volume vs the recent average</li>
+  <li><strong> __:</strong> Entering only <a href="https://fatneo.vercel.app/blog/tutorial-range">_TICKER_</a> (e.g. SPY ↩️) is the default command. It gets you a basic Options Implied price outlook for the week ahead</li>
+  <li><strong>💥 DOOM:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-doom">_TICKER_ DOOM</a> gets you the Options Implied probability of a strong drop in the month ahead</li>
+  <li><strong>🎲 KELLY:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-kelly">_TICKER_ KELLY</a> is an experimental feature, to do Options Implied Kelly optimal bet sizing. Perhaps one of the most overlooked tool by investors.</li>
+  <li><strong>📈 CALL:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-call">_TICKER_ CALL</a> is an experimental feature, to find the optimal covered call/call-spread to sell in the month ahead</li>
+  <li><strong>🔊 VOLUME:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-volume">_TICKER_ VOLUME</a> gets you a basic comparative view of stock volume vs the recent average</li>
   <li><strong>🎢 WSB:</strong> Entering _TICKER_ WSB is an experimental feature, offering a directional feel for sentiment on Reddit's WallStreetBets (e.g. Mentions)</li>
     <li><strong>📯 _USER_X_:</strong> We are just getting started and are all ears. Most of the skills above came from users like you. Connect below.</li>
 </ul>
