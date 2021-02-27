2a42ddf9c870a56d919e7d5955d252f886cb30df
Harmeet Goindi <goindi@gmail.com>
2021-02-26 22:51:25 -0500

    test

--- api/test.py
+++ api/test.py
@@ -93,11 +93,13 @@ def make_call_response(symbol, resp_dict):
             resp_dict['main_point'] = f'${best_call["strike"]} Strike Call Expiring on {best_call["expiry"]}'
             resp_dict['description'] = "Optimal covered call to sell"
             resp_dict['supporting_data'] = f'Now@ ${best_call["bid"]}'
+            resp_dict['main_class'] = 'bullish'
         else:
             resp_dict['main_point'] = f''
         if best_spread:
             resp_dict['secondary_point'] = f'Buy ${best_spread["strike_to_buy"]} and sell ${best_spread["strike_to_sell"]} call (expiring on {best_spread["expiry"]})'
             resp_dict['secondary_description'] = "Optimal call spread to sell given probalities implied by options"
+            resp_dict['secondary_class'] = 'bullish'
     return resp_dict
 
 #SKILL_MAP = {'range':'options/range/', 'ape':'options/kelly/','kelly':'options/kelly/','doom':'options/doom/' , 'volume':'stocks/volume/', 'prob_pct':'options/prob_pct/','wsb':'stocks/volume/', 'new2':'options/kelly/' }
