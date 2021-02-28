45ea9a8fd3657073babd267ba03101620b91ac44
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-02-28 10:35:44 -0600

    Update _posts.js

--- src/routes/blog/_posts.js
+++ src/routes/blog/_posts.js
@@ -15,20 +15,6 @@ const posts = [
 			<p><strong> Sign up for the <a href='https://oracled.mailchimpsites.com/'> DailySpread</a></strong></p>
 		`
   },
-  {
-      title: 'How do I use the tool?',
-      slug: 'tutorial-main',
-      html: `
-            <h1 style="color:blue;"> Type the symbol and hit enter or click Go</h1>
-            <figure style='width:80%;margin:0 auto'>
-            	<img alt='Type symbol and Go' src='FatNeo_Tutorial_1_1.png'>
-            </figure>
-            <h1 style="color:blue;"> Enjoy the simplicity </h1>
-            <figure style='width:80%;margin:0 auto'>
-            	<img alt='Enjoy the data' src='FatNeo_Tutorial_1_2.png'>
-            </figure>
-        `
-    },
     {
         title: 'What is range?',
         slug: 'tutorial-range',
