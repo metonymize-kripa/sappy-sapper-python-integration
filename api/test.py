f0c292bcb293c91da22ac3a3f80e5c068a6868e6
Harmeet Goindi <goindi@gmail.com>
2021-02-27 21:31:25 -0500

    div

--- api/test.py
+++ api/test.py
@@ -38,7 +38,7 @@ def make_div_response(symbol, resp_dict):
             resp_dict['main_point'] = "{symbol} has had no dividend. A shame"
         else:
             resp_dict['main_point'] = f'Last dividend: ${input_dict["div"]}'
-            resp_dict['description'] = f'Last Dividend Date : {datetime.strptime(best_call["div_date"], "%Y-%m-%d").strftime("%b %d")}'
+            resp_dict['description'] = f'Last Dividend Date : {datetime.strptime(input_dict["div_date"], "%Y-%m-%d").strftime("%b %d")}'
             if input_dict["div_yld"]:
                 resp_dict['supporting_data'] = f'Div Yield @ {round(100*input_dict["div_yld"])}%'
 
