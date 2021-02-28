df00c8b17e632279af3b9d56eaec8e518a5797f7
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-02-28 05:08:49 -0600

    Update skills_key.svelte

--- src/routes/skills_key.svelte
+++ src/routes/skills_key.svelte
@@ -4,19 +4,20 @@
 
 <h1>Skills, the Key</h1>
 
-<p>By a Fat Neo skill we mean something like "SPY _SKILL_ ↩️". It is how Fat Neo helps you trade faster, smarter, funner.</p>
+p>By a Fat Neo skill we mean something like "SPY SKILL ↩️". It is how Fat Neo helps you trade faster, smarter, funner.</p>
 <p>What are all the skills that Fat Neo has? How do they work? We try to answer these questions briefly on this page,
     with links to more detailed sources and methodology that you can find in the blog section. </p>
 
 <ul>
-  <li><strong> __:</strong> Entering only <a href="https://fatneo.vercel.app/blog/tutorial-range">_TICKER_</a> (e.g. SPY ↩️) is the default command. It gets you a basic Options Implied price outlook for the week ahead</li>
-  <li><strong>💥 DOOM:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-doom">_TICKER_ DOOM</a> gets you the Options Implied probability of a strong drop in the month ahead</li>
-  <li><strong>🎲 KELLY:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-kelly">_TICKER_ KELLY</a> is an experimental feature, to do Options Implied Kelly optimal bet sizing. Perhaps one of the most overlooked tool by investors.</li>
-  <li><strong>📈 CALL:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-call">_TICKER_ CALL</a> is an experimental feature, to find the optimal covered call/call-spread to sell in the month ahead</li>
-  <li><strong>🔊 VOLUME:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-volume">_TICKER_ VOLUME</a> gets you a basic comparative view of stock volume vs the recent average</li>
-  <li><strong>💵 DIV:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-div">_TICKER_ DIV</a> gets you a basic view of stock dividends</li>
-  <li><strong>🎢 WSB:</strong> Entering _TICKER_ WSB is an experimental feature, offering a directional feel for sentiment on Reddit's WallStreetBets (e.g. Mentions)</li>
-    <li><strong>📯 _USER_X_:</strong> We are just getting started and are all ears. Most of the skills above came from users like you. Connect below.</li>
+  <li><strong> BLANK:</strong> Entering only <a href="https://fatneo.vercel.app/blog/tutorial-range">TICKER</a> (e.g. SPY ↩️) is the default command. It gets you a basic Options Implied price outlook for the week ahead</li>
+  <li><strong>💥 DOOM:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-doom">TICKER DOOM</a> gets you the Options Implied probability of a strong drop in the month ahead</li>
+  <li><strong>🎲 KELLY:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-kelly">TICKER KELLY</a> is an experimental feature, to do Options Implied Kelly optimal bet sizing. Perhaps one of the most overlooked tool by investors.</li>
+  <li><strong>📈 CALL:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-call">TICKER CALL</a> is an experimental feature, to find the optimal covered call/call-spread to sell in the month ahead</li>
+  <li><strong>🔊 VOLUME:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-volume">TICKER VOLUME</a> gets you a basic comparative view of stock volume vs the recent average</li>
+  <li><strong>💵 DIV:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-div">TICKER DIV</a> gets you a basic view of stock dividends</li>
+  <li><strong>🎢 WSB:</strong> Entering TICKER WSB is an experimental feature, offering a directional feel for sentiment on Reddit's WallStreetBets (e.g. Mentions)</li>
+  <li><strong>🦉 WISE:</strong> Entering TICKER WISE is an experimental feature, offering a directional feel of a curated group of experts who Tweet </li>
+    <li><strong>📯 USER_X:</strong> We are just getting started and are all ears. Most of the skills above came from users like you. Connect below.</li>
 </ul>
 
 <p><strong>Test and learn feedback:<a href='https://oracled.mailchimpsites.com/'>here</a></strong></p>
