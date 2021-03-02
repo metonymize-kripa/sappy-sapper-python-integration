042711d6af561a41b1db36ab9d1802e64f222dbd
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-03-02 10:55:30 -0600

    Update skills_key.svelte

--- src/routes/skills_key.svelte
+++ src/routes/skills_key.svelte
@@ -8,16 +8,22 @@
 <p>What are all the skills that Fat Neo has? How do they work? We try to answer these questions briefly on this page,
     with links to more detailed sources and methodology that you can find in the blog section. </p>
 
+<h2>Fast Data</h2>
+<p>Optimized already for speed of execution.</p>
 <ul>
   <li><strong> BLANK:</strong> Entering only <a href="https://fatneo.vercel.app/blog/tutorial-range">TICKER</a> (e.g. SPY ↩️) is the default command. It gets you a basic Options Implied price outlook for the week ahead</li>
   <li><strong>💥 DOOM:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-doom">TICKER DOOM</a> gets you the Options Implied probability of a strong drop in the month ahead</li>
   <li><strong>🎲 KELLY:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-kelly">TICKER KELLY</a> is an experimental feature, to do Options Implied Kelly optimal bet sizing. Perhaps one of the most overlooked tool by investors.</li>
-  <li><strong>📈 CALL:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-call">TICKER CALL</a> is an experimental feature, to find the optimal covered call/call-spread to sell in the month ahead</li>
   <li><strong>🔊 VOLUME:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-volume">TICKER VOLUME</a> gets you a basic comparative view of stock volume vs the recent average</li>
   <li><strong>💵 DIV:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-div">TICKER DIV</a> gets you a basic view of stock dividends</li>
   <li><strong>🎢 WSB:</strong> Entering TICKER WSB is an experimental feature, offering a directional feel for sentiment on Reddit's WallStreetBets (e.g. Mentions)</li>
+</ul>	
+<h2>Slow Data</h2>
+<p>Sluggish, relying on large amounts of JIT data collection that needs future optimization.</p>
+<ul>
+  <li><strong>📈 CALL:</strong> Entering <a href="https://fatneo.vercel.app/blog/tutorial-call">TICKER CALL</a> is an experimental feature, to find the optimal covered call/call-spread to sell in the month ahead</li>
   <li><strong>🦉 WISE:</strong> Entering TICKER WISE is an experimental feature, offering a directional feel of a curated group of experts who Tweet </li>
-    <li><strong>📯 USER_X:</strong> We are just getting started and are all ears. Most of the skills above came from users like you. Connect below.</li>
+   <li><strong>📯 USER_X:</strong> We are just getting started and are all ears. Most of the skills above came from users like you. Connect below.</li>
 </ul>
 
 <p><strong>Test and learn feedback:<a href='https://oracled.mailchimpsites.com/'>here</a></strong></p>
