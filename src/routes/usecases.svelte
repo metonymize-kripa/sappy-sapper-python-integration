0d727c98dbb3661ffe4a5d355e002013ecacd69f
Harmeet Goindi <goindi@gmail.com>
2021-03-07 19:16:51 -0500

    updates

--- src/routes/usecases.svelte
+++ src/routes/usecases.svelte
@@ -46,9 +46,9 @@ Here are a few use cases to inspire your exploration with our tools. Be bold. Ho
     </div>
     <div class="card col-6 bg-light" >
       <header>
-        <h4>Generating 20% yield from MSFT</h4>
+        <h4>Making money on TSLA meltdown</h4>
       </header>
-        You heard it right. Using fatneo, one of our users has been writing weekly covered calls on MSFT. He looks at the potential <a  href="https://wtfday.fatneo.com/?symbol=msft&cmd=">one week range of MSFT</a> and writes weekly call outside that range.  He has made about $400 in a month (20% yield). If he keeps doing it for a year, he could make over $5,000.
+        TSLA has had a bad run during Feb. It fell over 30%. One of our users believed that the stock had hit the bottom. Using <a  href="/?symbol=tsla&cmd=kelly">fatneo's Kelly skill he knew the probability of upside is more than 50%. The volatility was high and so he sold a weekly $520 put and made over $1,000.
     </div>
 </div>
 
