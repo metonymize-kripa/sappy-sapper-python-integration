009e9895cad0beb5e95b28ca0547340599e9fd8a
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-03-11 08:46:11 -0600

    added lonny's use case

--- src/routes/usecases.svelte
+++ src/routes/usecases.svelte
@@ -48,3 +48,18 @@ Here are a few use cases to inspire your exploration with our tools. Be bold. Ho
         TSLA has had a bad run during Feb. It fell over 30%. One of our users believed that the stock had hit the bottom. Using <a  href="/?symbol=tsla&cmd=kelly">fatneo's Kelly skill</a> he knew the probability of upside is more than 50%. The volatility was high and so he sold a weekly $520 put and made over $1,000.
     </div>
 </div>
+<div class="row">
+    <div class="card col-6 bg-light" >
+      <header>
+        <h4>Profiting from the DailySpread</h4>
+      </header>
+        One of our users just shared a way to use our Kelly picks from the <a href="https://oracled.mailchimpsites.com/">DailySpread</a> to make money in a way that hadn't really occured to us until he shared it.
+	Here's the tip, in his own words <it>"just because of my positions (i.e. didn't have the stock to call) but have been buying at, holding "until" and selling at the [covered]call position and it's worked 5 out of 5 times"</it>
+    </div>
+    <div class="card col-6 bg-light" >
+      <header>
+        <h4>Your use case here!</h4>
+      </header>
+        There's many more where these came from. Please email us at the <a href="https://oracled.mailchimpsites.com/">DailySpread</a>. We're excited to build for you.
+    </div>
+</div>
