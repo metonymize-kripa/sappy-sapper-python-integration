cbea6c101441aae74c7d817049b9e81f8751784f
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-03-12 07:18:24 -0600

    current sell list

--- src/routes/sell.svelte
+++ src/routes/sell.svelte
@@ -23,7 +23,7 @@ body {
     }
 </style>
 <script>
-    let symbol_list = ["CVNA","AMC","SKT","GME","GVIP"];
+    let symbol_list = ["IAC", "PLTR", "BB","CVNA","GME", "SKT", "AMC"];
     let table_list= [];
     let table_show = [];
     for(var i = 0; i < symbol_list.length; i++)
