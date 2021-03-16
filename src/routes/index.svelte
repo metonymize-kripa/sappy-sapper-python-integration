e860e11171b76ce6cb460057f972838bf4e598ee
Harmeet Goindi <goindi@gmail.com>
2021-03-15 20:39:13 -0400

    tags

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -126,7 +126,11 @@
             <h2> ☝️ Symbol+skill+↵ </h2>
         {:else}
             <div style="padding:0 1rem;" class='bd-dark text-center'>
-                <a class="text-white bg-primary bd-dark" on:click={getAPIData("put",api_output.symbol)}>{tag1}</a><h2 class="{api_output.main_class}">{ api_output.main_point}</h2><a class="text-white bg-primary bd-dark" on:click={getAPIData("call",api_output.symbol)}>{tag2}</a>
+            {#if tag1 != ""}
+                <h2 class="{api_output.main_class}"><a class="text-white bg-primary bd-dark" on:click={getAPIData("put",api_output.symbol)}>{tag1}</a>{ api_output.main_point}<a class="text-white bg-primary bd-dark" on:click={getAPIData("call",api_output.symbol)}>{tag2}</a></h2>
+            {:else}
+                <h2 class="{api_output.main_class}">{ api_output.main_point}</h2>
+            {/if}
                 <p>{@html api_output.description}</p>
                 <h3 class="supporting">{api_output.supporting_data}</h3>
             </div>
