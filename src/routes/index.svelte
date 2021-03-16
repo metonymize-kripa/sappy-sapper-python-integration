28c7db74617be4a4b9356e3d3497beb6e581e04a
Harmeet Goindi <goindi@gmail.com>
2021-03-16 18:52:11 -0400

    tags'

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -126,9 +126,9 @@
             <h2> ☝️ Symbol+skill+↵ </h2>
         {:else}
             <div style="padding:0 1rem;" class='bd-dark text-center'>
-            <h2 style ="margin:1rem 0 -0.5rem 0;" class="{api_output.main_class}">{ api_output.main_point}</h2>
+            <h2 style ="margin:1rem 0px -0.5rem 0px;" class="{api_output.main_class}">{ api_output.main_point}</h2>
             {#if tag1 != ""}
-                <h3><a class="text-white bg-primary bd-dark" style="margin:0 2rem; font-size:1.5rem;" href='' on:click={getAPIData("put",api_output.symbol)}>{tag1}</a><a class="text-white bg-primary bd-dark" style='margin:0 2rem; font-size:1.5rem;' href='' on:click={getAPIData("call",api_output.symbol)}>{tag2}</a></h3>
+                <a class="text-white bg-primary bd-dark" style="margin:0 2rem; font-size:1.5rem;" href='' on:click={getAPIData("put",api_output.symbol)}>{tag1}</a><a class="text-white bg-primary bd-dark" style='margin:0 2rem; font-size:1.5rem;' href='' on:click={getAPIData("call",api_output.symbol)}>{tag2}</a>
             {:else}
                 <h2 class="{api_output.main_class}">{ api_output.main_point}</h2>
             {/if}
