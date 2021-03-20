d08b5bd0bcf40d1fb1c943516db650c46cbf0521
Harmeet Goindi <goindi@gmail.com>
2021-03-20 18:10:59 -0400

    pani

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -51,13 +51,18 @@
 <nav>
 	<ul>
 		<li><a class='{segment === undefined ? "selected" : ""}' href='.'>Home</a></li>
-	<!--	<li><a class='{segment === "about" ? "selected" : ""}' href='about'>About</a></li>
+        <li><a class='{segment === "portfolio" ? "selected" : ""}' href='portfolio'>My Portfolio</a></li>
+
+<!--    <li><a class='{segment === "about" ? "selected" : ""}' href='about'>About</a></li>
 		<li><a class='{segment === "skills_key" ? "selected" : ""}' href='skills_key'>Skills</a></li>
-		<li><a class='{segment === "usecases" ? "selected" : ""}' href='usecases'>Usecases</a></li> -->
-        <li><a class='{segment === "alerts" ? "selected" : ""}' href='alerts'>Alerts</a></li>
+		<li><a class='{segment === "usecases" ? "selected" : ""}' href='usecases'>Usecases</a></li>
+       <li><a class='{segment === "alerts" ? "selected" : ""}' href='alerts'>Alerts</a></li>
         <li><a class='{segment === "buy" ? "selected" : ""}' href='buy '>Buy</a></li>
         <li><a class='{segment === "sell" ? "selected" : ""}' href='sell '>Sell</a></li>
         <li><a class='{segment === "pricing" ? "selected" : ""}' href='pricing'>Pricing</a></li>
+-->
+
+
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
