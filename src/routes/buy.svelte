6ec0f92919d80fd8d7b8cebb662766db37e74968
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-03-23 11:04:41 -0500

    mar 23 midday

--- src/routes/buy.svelte
+++ src/routes/buy.svelte
@@ -27,7 +27,8 @@
     const { preloading, page, session } = stores();
 	//let symbol_list = ["LBRDK","IAC","TWTR","SE","MELI","GME","UWMC","SPY","AAPL","AMC","PLTR","RKT"];
 	//let symbol_list = ["LBRDK","IAC","TWTR","SE","MELI","GME","SPY","AMC","AAPL","UWMC","PLTR","TSLA"];
-	let symbol_list = ["LBRDK","IAC","TWTR","SE","MELI","GME","AMC","TSLA","SPY","PLTR","AAPL","UWMC"];
+	//let symbol_list = ["LBRDK","IAC","TWTR","SE","MELI","GME","AMC","TSLA","SPY","PLTR","AAPL","UWMC"];
+	let symbol_list = ["LBRDK","IAC","TWTR","SE","MELI","GME","AMC","PLTR","SPY","TSLA","UPST","AAPL"];
     //let table_dict= {};
     let table_list=[];
     let table_show = [];
