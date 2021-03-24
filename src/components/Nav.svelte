f43a2f369bfb4a430438e673c007f06a5836a14c
metonymize-kripa <kripa@metonymize.com>
2021-03-24 10:47:37 -0500

    separated about

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -51,5 +51,6 @@
 <nav>
 	<ul>
 		<li><a class='{segment === undefined ? "selected" : ""}' href='.'>home</a></li>
+    <li><a class='{segment === "about" ? "selected" : ""}' href='about'>about</a>>
 	</ul>
 </nav>
