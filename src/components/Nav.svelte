3c11b96e7029908d3d689f7b1ead92691762633f
metonymize-kripa <kripa@metonymize.com>
2021-03-24 10:51:19 -0500

    header fix

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -51,6 +51,6 @@
 <nav>
 	<ul>
 		<li><a class='{segment === undefined ? "selected" : ""}' href='.'>home</a></li>
-    <li><a class='{segment === "about" ? "selected" : ""}' href='about'>about</a>>
+    <li><a class='{segment === "about" ? "selected" : ""}' href='about'>about</a></li>
 	</ul>
 </nav>
