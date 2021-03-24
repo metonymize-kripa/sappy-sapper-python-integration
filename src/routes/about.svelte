f06dc7910d9c4b1ae90367d55b458868537770ca
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-03-24 11:06:38 -0500

    source and size

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -16,7 +16,7 @@
         <h1>Tactical Allocation for the Social Trader</h1>
         <p>Retail investors now account for almost one-third of market activity. Tactical trading has gone mainstream, and fractional.</p>
         <p>Social traders on platforms like Robinhood and Public are trading in ways that previously only large sophisticated investors could do (e.g. Gamma+Short Squeeze on GME). They need new tactical tools, enabled with Options data and Social intelligence. Oracled.com helps social traders make tactical moves like the pros.</p>
-        <p>The tool helps size one week out tactical trades, by ticker and portfolio value. Learn more below about how Fractional Kelly sizing and Options data makes tactical trading smarter and safer.</p>
+        <p>The tool helps source and size one week out tactical trades, by ticker and portfolio value. Learn more below about how Fractional Kelly sizing and Options data makes tactical trading smarter and safer.</p>
 </div>
 
 
