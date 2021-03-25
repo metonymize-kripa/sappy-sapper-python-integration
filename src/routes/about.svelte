5a7ceac9df8a642e2622713bb49a41ed89cb5f89
metonymize-kripa <kripa@metonymize.com>
2021-03-25 15:26:53 -0500

    fixed million to billion

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -14,7 +14,7 @@
 
 <div class="row">
         <h1>Tactical Speed for the Crypto Trader</h1>
-        <p>In the fourth quarter of 2019, Coinbase reported $5 billion in retail trading volume matched with $9 million in institutional volume. Fast forward to the fourth quarter of 2020 and the exchange reported $32 billion in retail volume – a 540% increase – matched with $57 billion from institutional clients, a 533% increase. Crypto is going mainstream.</p>
+        <p>In the fourth quarter of 2019, Coinbase reported $5 billion in retail trading volume matched with $9 billion in institutional volume. Fast forward to the fourth quarter of 2020 and the exchange reported $32 billion in retail volume – a 540% increase – matched with $57 billion from institutional clients, a 533% increase. Crypto is going mainstream.</p>
         <p>Crypto traders need tactical tools, enabled with AI analytics and Social intelligence. Oracled.com helps crypto traders make tactical moves like the pros.</p>
         <p>The tool helps size one week out tactical trades, across popular exchange traded crypto currencies. Sign up for personalized alerts to make your tactical Crypto trading smarter and safer.</p>
 </div>
