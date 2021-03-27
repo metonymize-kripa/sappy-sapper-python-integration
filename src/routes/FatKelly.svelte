36ca424b50cb613ec2a2ad2574aefd4462b75ded
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-03-27 12:02:09 -0500

    tweaks

--- src/routes/FatKelly.svelte
+++ src/routes/FatKelly.svelte
@@ -1,9 +1,10 @@
 <h2 style="text-align: left;">Why?</h2>
-<div style="text-align: left;">The FatKelly answers the most important question of any Fractional Share Trader:&nbsp;<strong><span style="display: inline-block;"><a href="https://social.oracled.com">How much should I buy?</a></span></strong></div>
+<div style="text-align: left;">The FatKelly answers the most important question for any Fractional Share Trader:&nbsp;<strong><span style="display: inline-block;"><a href="https://social.oracled.com">How much should I buy?</a></span></strong></div>
 <div style="text-align: left;">It is simple, and provably makes betting/investing better. Don&rsquo;t take just out word for it:&nbsp;<span style="display: inline-block;"><a href="https://onlinelibrary.wiley.com/doi/abs/10.1002/9781119020516.ch4">Warren Buffett</a></span>,&nbsp;<span style="display: inline-block;"><a href="https://www.gurufocus.com/news/4883/investing-with-the-kelly-criterion">Charlie Munger</a></span>,&nbsp;<span style="display: inline-block;"><a href="https://www.wiley.com/en-us/The+Dhandho+Investor%3A+The+Low+Risk+Value+Method+to+High+Returns-p-9780470043899">Mohnish Pabrai</a></span>, and&nbsp;<span style="display: inline-block;"><a href="https://25iq.com/2014/09/26/a-dozen-things-ive-learned-from-billl-gross/">Bill Gross</a></span>, and other impressive financial types love it too.</div>
 
 <h2 style="text-align: left;">How does it work?</h2>
 <div style="text-align: left;">The Kelly criterion is a time-tested formula for sizing bets or investments. The idea is simple, and has been called&nbsp;<em><span style="display: inline-block;"><a href="https://www.amazon.com/Fortunes-Formula-Scientific-Betting-Casinos/dp/B072VX7DCS">Fortune&rsquo;s Formula</a></span></em>: Given a sum of money to invest, allocate it in proportion to your&nbsp;<strong>edge/odds</strong> (The edge is the&nbsp;<em>amount</em> of gain Vs loss. The odds, the&nbsp;<em>chances of</em> a gain Vs loss).&nbsp;</div>
+<br>
 <div style="text-align: left;">The FatKelly, is our extension of the traditional Kelly criterion. With two modifications:</div>
 <ol>
     <li style="text-align: left; list-style-type: decimal;">We use Options data implied win/loss probabilities and expected returns, and</li>
@@ -15,12 +16,12 @@
 <ol>
     <li style="text-align: left; list-style-type: decimal;">Consider how much money you have to invest (the purse/cash you&rsquo;re willing to risk without FDIC insurance)</li>
     <li style="text-align: left; list-style-type: decimal;">Source and screen available investments/bets on the basis of edge and odds (Options &nbsp;data can help)</li>
-    <li style="text-align: left; list-style-type: decimal;">Pick the ones you like and use some type of modified Kelly to size the bet given your purse size</li>
+    <li style="text-align: left; list-style-type: decimal;">Pick the ones you like and use some type of modified Kelly to size the bet given your purse</li>
 </ol>
 <div style="text-align: left;">Check out the&nbsp;<span style="display: inline-block;"><a href="https://social.oracled.com/">💎 Oracle</a></span> if you like. It is a tool we made for ourselves and friends to make these steps easier.</div>
 
 <h2 style="text-align: left;">Where can I learn more? (References)</h2>
-<div style="text-align: left;">There is math, but it is surprisingly simple. Examples and some simple formulas can be found in the references below and the others they cite.&nbsp;</div>
+<div style="text-align: left;">There is some math, but it is surprisingly simple. Examples and some simple formulas can be found in the references below and the others they cite.&nbsp;</div>
 <ol>
     <li style="text-align: left; list-style-type: decimal;"><span style="display: inline-block;"><a href="https://blogs.cfainstitute.org/investor/2018/06/14/the-kelly-criterion-you-dont-know-the-half-of-it/">The Kelly Criterion: You Don&rsquo;t Know the Half of It, CFA Institute</a></span></li>
     <li style="text-align: left; list-style-type: decimal;"><span style="display: inline-block;"><a href="https://www.globalcapital.com/article/k6543wh6f19l/option-prices-imply-a-probability-distribution">Option Prices Imply A Probability Distribution, Global Capital</a></span></li>
