5d4c5eac978c25933f3caef192c4b131249d0fc0
Harmeet Goindi <goindi@gmail.com>
2021-04-01 18:42:29 -0400

    riskreturn

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -177,23 +177,23 @@ function copyurl(my_url) {
                 <button class="text-white bg-dark" style="margin:0 0 2rem 0 ;padding:0.5rem; font-size:1.25rem;" on:click={updateClipboard((my_kelly*portfolio_size).toFixed(2))}>copy and trade@RH</button>
 
                 <br>
-                <a href="https://reddit.com/submit?url={post_url}{ticker}&title={post_title}" class="fa fa-reddit"></a>
-                <a href="https://twitter.com/share?url={post_url}{ticker}&text={post_title}&hashtags=kelly,fatneo" class="fa fa-twitter"></a>
-                <a href="https://api.whatsapp.com/send?text={post_title} {post_url}{ticker}" class="fa fa-whatsapp"></a>
-                <a href="" on:click={copyurl("https://social.oracled.com/?symbol="+ticker)} class="fa fa-copy"></a>
-                <!-- <a href="https://mail.google.com/mail/u/1/?fs=1&su={post_title}{ticker}&tf={post_url}?symbol={ticker}" class="fa fa-envelope"></a> -->
+
             </div>
             <div class="col-3" >
                 Risk: <button class="button error pull-right" style="width:7rem; padding:0.4rem 0.5rem">{(Math.pow( ((1-api_output.kelly2)/(1+api_output.kelly2)), (1/my_kelly) )*100).toFixed(2)}%</button>
                 <br>
                 <br>
                 Return: <button class="button success pull-right" style="width:7rem; padding:0.4rem 0.5rem">${(100*my_kelly*my_kelly).toFixed(2)}</button>
-                <br>
-                <br>
-                <br>
-                <button class="button dark pull-right" on:click={goback}>Go Back</button>
+
+
             </div>
         </div>
+        <a href="https://reddit.com/submit?url={post_url}{ticker}&title={post_title}" class="fa fa-reddit"></a>
+        <a href="https://twitter.com/share?url={post_url}{ticker}&text={post_title}&hashtags=kelly,fatneo" class="fa fa-twitter"></a>
+        <a href="https://api.whatsapp.com/send?text={post_title} {post_url}{ticker}" class="fa fa-whatsapp"></a>
+        <a href="" on:click={copyurl("https://social.oracled.com/?symbol="+ticker)} class="fa fa-copy"></a>
+        <!-- <a href="https://mail.google.com/mail/u/1/?fs=1&su={post_title}{ticker}&tf={post_url}?symbol={ticker}" class="fa fa-envelope"></a> -->
+        <button class="button dark pull-right" on:click={goback}>Go Back</button>
         {/if}
     </div>
     {/if}
