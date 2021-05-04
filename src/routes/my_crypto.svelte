091a4f787261b05b07155398175e46fe3c06229e
Harmeet <goindi@gmail.com>
2021-05-04 11:27:59 -0400

    st

--- src/routes/my_crypto.svelte
+++ src/routes/my_crypto.svelte
@@ -32,7 +32,7 @@
     let table_show = [];
     for(var i = 0; i < symbol_list.length; i++)
     {
-        table_list.push({"symbol":symbol_list[i],"price":"NA","range":"NA","volume_pct":"NA","wedge":"NA","ascending_triangle ":"NA"});
+        table_list.push({"symbol":symbol_list[i],"price":"NA","range":"NA","volume_pct":"NA","wedge":"NA","ascending_triangle ":"NA","wedge_st":"NA","ascending_triangle_st":"NA"});
     }
 	function currencyFormat(num,decimals) {
 	 return num.toFixed(decimals).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
@@ -76,12 +76,16 @@
 							table_list[k].range = '$'+(my_dict.low_range).toFixed(6)+" - $"+(my_dict.high_range).toFixed(6);
 						}
 						table_list[k].volume_pct = (my_dict.today_volume/my_dict.avg_10d_volume).toFixed(2);
-                        table_list[k].wedge = (my_dict.low_slope - my_dict.high_slope);
+
+						table_list[k].wedge = (my_dict.low_slope - my_dict.high_slope);
                         if ( (my_dict.low_slope > my_dict.high_slope) && my_dict.high_slope <0.0001)
                             table_list[k].wedge += 0.2;
                         table_list[k].wedge = (table_list[k].wedge*1000).toFixed(2);
                         if (table_list[k].wedge <0)
                             table_list[k].wedge = 0;
+						else if (table_list[k].wedge > 100)
+	                            table_list[k].wedge = 100;
+
                         if ( my_dict.high_slope <0.01 &&  my_dict.high_slope > 0.01)
                             {
                                 if (my_dict.low_slope>0.02)
@@ -90,7 +94,30 @@
                         else{table_list[k].ascending_triangle = -1+my_dict.low_slope-my_dict.high_slope;}
                          if (table_list[k].ascending_triangle <0)
                             table_list[k].ascending_triangle = 0;
-                        
+						else if (table_list[k].ascending_triangle > 1)
+							   table_list[k].ascending_triangle = 1;
+
+						table_list[k].wedge_st = (my_dict.low_slope_st - my_dict.high_slope_st);
+                        if ( (my_dict.low_slope_st > my_dict.high_slope_st) && my_dict.high_slope_st <0.0001)
+                            table_list[k].wedge_st += 0.2;
+                        table_list[k].wedge_st = (table_list[k].wedge_st*1000).toFixed(2);
+                        if (table_list[k].wedge_st <0)
+                            table_list[k].wedge_st = 0;
+						else if (table_list[k].wedge_st > 100)
+                            table_list[k].wedge_st = 100;
+
+                        if ( my_dict.high_slope_st <0.01 &&  my_dict.high_slope_st > 0.01)
+                            {
+                                if (my_dict.low_slope_st>0.02)
+                                    table_list[k].ascending_triangle_st = 0.5+my_dict.low_slope_st*10;
+                            }
+                        else{table_list[k].ascending_triangle_st = -1+my_dict.low_slope_st-my_dict.high_slope_st;}
+                         if (table_list[k].ascending_triangle_st <0)
+                            table_list[k].ascending_triangle_st = 0;
+						else if (table_list[k].ascending_triangle_st >1)
+							table_list[k].ascending_triangle_st = 1;
+
+
                     }
                 }
               });
@@ -114,12 +141,14 @@ if (process.browser)
           <th class="no-emphasis">Vol🔱</th>
 		  <th class="emphasis">Wedge🏁</th>
 		  <th class="emphasis">🔼📐</th>
+		  <th class="emphasis">ST Wedge🏁</th>
+		 <th class="emphasis">ST 🔼📐</th>
           <th class="no-emphasis">Chart</th>
         </tr>
     </thead>
     <tbody>
-        {#each table_list as {symbol,price,range,volume_pct,wedge,ascending_triangle}, i}
-            
+        {#each table_list as {symbol,price,range,volume_pct,wedge,ascending_triangle,wedge_st,ascending_triangle_st}, i}
+
             <tr >
                 <a href="https://www.tradingview.com/symbols/{symbol.replace('-','')}" target="_blank" ><td class="emphasis"> {symbol}</td></a>
                 <td class="no-emphasis">${price}</td>
@@ -127,9 +156,11 @@ if (process.browser)
                 <td class="no-emphasis">{volume_pct}</td>
 				<td class="emphasis">{wedge}%</td>
 				<td class="emphasis">{(ascending_triangle*100).toFixed(2)}%</td>
+				<td class="emphasis">{wedge_st}%</td>
+				<td class="emphasis">{(ascending_triangle_st*100).toFixed(2)}%</td>
 				<a href="https://www.tradingview.com/symbols/{symbol.replace('-','')}" target="_blank" ><td class="no-emphasis">Chart</td>
             </tr>
-            
+
 
         {/each}
     </tbody>
@@ -139,10 +170,10 @@ if (process.browser)
     Fetch/Sort
 </button>
 <br><br><br>
-🧰 - Most likely one week range given past month performance 
+🧰 - Most likely one week range given past month performance
 <br>
-🔼📐- Chance that the coin is in Ascending Triangle Formation. Long Term Foramtion (one month) 
+🔼📐- Chance that the coin is in Ascending Triangle Formation. Long Term Foramtion (one month)
 <br>
-🏁 - Chance that the coin is in Downward Wedge Formation. Long Term Foramtion (one month). 
+🏁 - Chance that the coin is in Downward Wedge Formation. Long Term Foramtion (one month).
 <br>
 🔱 - Today's volume as a ratio of 10 day average
